c99c9d37c900c98d8761705b497c676947ccf564
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-30 13:00:59 -0700

    fixes

--- server.cc
+++ server.cc
@@ -36,7 +36,7 @@ void next_batch_req_handler(erpc::ReqHandle *req_handle, void *) {
   } else {
     auto &resp = req_handle->pre_resp_msgbuf_;
     rpc->resize_msg_buffer(&resp, kSmallMsgSize);
-    sprintf(reinterpret_cast<char *>(resp.buf_), "done");
+    sprintf(reinterpret_cast<char *>(resp.buf_), "success");
     rpc->enqueue_response(req_handle, &resp);
   }
 }
