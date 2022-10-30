33d7fd9cb840bac2408d24b49958fd349cf96894
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-30 12:41:30 -0700

    fixes

--- common.h
+++ common.h
@@ -5,7 +5,7 @@ static const std::string kServerHostname = "node1";
 static const std::string kClientHostname = "node0";
 
 static constexpr uint16_t kUDPPort = 31850;
-static constexpr uint8_t kInitRpc = 2;
-static constexpr uint8_t kNextBatchRpc = 3;
+static constexpr uint8_t kInitRpc = 3;
+static constexpr uint8_t kNextBatchRpc = 4;
 static constexpr size_t kSmallMsgSize = 16;
 static constexpr size_t kLargeMsgSize = 1024*1024;
