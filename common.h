b054a13e9cef1efb7ed8db3fd73066c2058dcb51
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-30 12:46:21 -0700

    fixes

--- common.h
+++ common.h
@@ -8,4 +8,4 @@ static constexpr uint16_t kUDPPort = 31850;
 static constexpr uint8_t kInitRpc = 3;
 static constexpr uint8_t kNextBatchRpc = 4;
 static constexpr size_t kSmallMsgSize = 16;
-static constexpr size_t kLargeMsgSize = 1024*1024;
+static constexpr size_t kLargeMsgSize = 1024*1024*2;
