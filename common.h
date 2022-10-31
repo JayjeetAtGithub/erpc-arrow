4355d233be57865df6ddee811d1a539e52eefc65
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-31 14:27:13 -0700

    fixes

--- common.h
+++ common.h
@@ -1,4 +1,7 @@
 #include <iostream>
+
+#include <arrow/schema.h>
+
 #include "rpc.h"
 
 static const std::string kServerHostname = "node1";
@@ -9,3 +12,23 @@ static constexpr uint8_t kInitRpc = 3;
 static constexpr uint8_t kNextBatchRpc = 4;
 static constexpr size_t kSmallMsgSize = 1;
 static constexpr size_t kLargeMsgSize = 1024*1024*2;
+
+auto schema = arrow::schema({
+    arrow::field("VendorID", arrow::int64()),
+    arrow::field("tpep_pickup_datetime", arrow::timestamp(arrow::TimeUnit::MICRO)),
+    arrow::field("tpep_dropoff_datetime", arrow::timestamp(arrow::TimeUnit::MICRO)),
+    arrow::field("passenger_count", arrow::int64()),
+    arrow::field("trip_distance", arrow::float64()),
+    arrow::field("RatecodeID", arrow::int64()),
+    arrow::field("store_and_fwd_flag", arrow::utf8()),
+    arrow::field("PULocationID", arrow::int64()),
+    arrow::field("DOLocationID", arrow::int64()),
+    arrow::field("payment_type", arrow::int64()),
+    arrow::field("fare_amount", arrow::float64()),
+    arrow::field("extra", arrow::float64()),
+    arrow::field("mta_tax", arrow::float64()),
+    arrow::field("tip_amount", arrow::float64()),
+    arrow::field("tolls_amount", arrow::float64()),
+    arrow::field("improvement_surcharge", arrow::float64()),
+    arrow::field("total_amount", arrow::float64())
+});
