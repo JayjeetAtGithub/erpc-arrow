2319adce7ea74d34e3ad984aed25f61017cf7247
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-31 17:31:34 -0700

    fixes

--- server.cc
+++ server.cc
@@ -27,10 +27,10 @@ void next_batch_req_handler(erpc::ReqHandle *req_handle, void *) {
     size_t num_bytes = data_buff->size();
 
     auto &resp = req_handle->dyn_resp_msgbuf_;
-    resp  =  rpc->alloc_msg_buffer(num_bytes);
+    resp  =  rpc->alloc_msg_buffer(1024*1024);
     
-    rpc->resize_msg_buffer(&resp, num_bytes);
-    memcpy(resp.buf_, data_buff->data(), num_bytes);
+    // rpc->resize_msg_buffer(&resp, 1024*1024);
+    memcpy(resp.buf_, data_buff->data(), 1024*1024);
     rpc->enqueue_response(req_handle, &resp);
     rpc->free_msg_buffer(resp);
   } else {
